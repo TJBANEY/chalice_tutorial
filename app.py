090c2f8f0c85e596a9c87f0ba7288d6f783b121b
Timothy Baney <baney-surv@hotmail.com>
2018-03-13 17:48:59 -0500

    remove .chalice file

--- app.py
+++ app.py
@@ -1,64 +1,29 @@
-from chalice import Chalice, BadRequestError
+from chalice import Chalice
 
-'''
-# Exception Types
-# * BadRequestError - return a status code of 400
-# * UnauthorizedError - return a status code of 401
-# * ForbiddenError - return a status code of 403
-# * NotFoundError - return a status code of 404
-# * ConflictError - return a status code of 409
-# * UnprocessableEntityError - return a status code of 422
-# * TooManyRequestsError - return a status code of 429
-# * ChaliceViewError - return a status code of 500
-'''
+app = Chalice(app_name='helloworldd')
 
-app = Chalice(app_name='chalice-helloworld')
-
-app.debug = True
-
-CITIES_TO_STATE = {
-    'seattle': 'WA',
-    'portland': 'OR',
-}
 
 @app.route('/')
 def index():
     return {'hello': 'world'}
 
-@app.route('/resource/{value}', methods=['PUT'])
-def put_test(value):
-    return {"value": f"PUT {value}"}
 
-@app.route('/resource/{value}', methods=['GET'])
-def put_test(value):
-    return {"value": f"GET {value}"}
-
-@app.route('/resource/{value}', methods=['POST'])
-def put_test(value):
-    return {"value": f"POST {value}"}
-
-'''
-# Current Requests
-current_request.query_params - A dict of the query params for the request.
-current_request.headers - A dict of the request headers.
-current_request.uri_params - A dict of the captured URI params.
-current_request.method - The HTTP method (as a string).
-current_request.json_body - The parsed JSON body (json.loads(raw_body))
-current_request.raw_body - The raw HTTP body as bytes.
-current_request.context - A dict of additional context information
-current_request.stage_vars - Configuration for the API Gateway stage
-'''
-
-@app.route('/route_w_requests/')
-def route_w_requests():
-    request = app.current_request
-
-    return {"value": request}
-
-@app.route('/cities/{city}')
-def state_of_city(city):
-    try:
-        return {'state': CITIES_TO_STATE[city]}
-    except KeyError:
-        raise BadRequestError("Unknown city '%s', valid choices are: %s" % (
-            city, ', '.join(CITIES_TO_STATE.keys())))
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#

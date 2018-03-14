dc66ce7eed03bfe5713aea0459741253874f8b56
Timothy Baney <baney-surv@hotmail.com>
2018-03-13 20:17:23 -0500

    render html for index route

--- app.py
+++ app.py
@@ -1,29 +1,47 @@
-from chalice import Chalice
+from chalice import Chalice, BadRequestError, Response
+import jinja2
+import os
 
-app = Chalice(app_name='helloworlddd')
+from os import listdir
+from os.path import isfile, join
 
+app = Chalice(app_name='chalice-helloworld')
+
+app.debug = True
+
+battleship_template = '''
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12" style="height: 200px; border: 1px solid red;">
+          Woot Woot This Works
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+'''
+
+def render(tpl_path, context):
+    path, filename = os.path.split(tpl_path)
+    return jinja2.Environment(
+        loader=jinja2.FileSystemLoader(path or './')
+    ).get_template(filename).render(context)
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
 
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    return Response(body=battleship_template,
+                    status_code=200,
+                    headers={'Content-Type': 'text/html'})
